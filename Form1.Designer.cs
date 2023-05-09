19dc12a89b66a470c93ab51a6622676d2c8a6e8d
Makk1aud <1roman080283@mail.ru>
2023-05-09 14:56:45 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Graphic_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPctBox = new System.Windows.Forms.PictureBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.blackWhite = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPctBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPctBox
+            // 
+            this.mainPctBox.Location = new System.Drawing.Point(24, 25);
+            this.mainPctBox.Name = "mainPctBox";
+            this.mainPctBox.Size = new System.Drawing.Size(356, 358);
+            this.mainPctBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainPctBox.TabIndex = 0;
+            this.mainPctBox.TabStop = false;
+            this.mainPctBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPctBox_MouseDown);
+            this.mainPctBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainPctBox_MouseMove);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(24, 389);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(107, 25);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Открыть";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(273, 389);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(107, 25);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Сохранить";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // blackWhite
+            // 
+            this.blackWhite.Location = new System.Drawing.Point(150, 389);
+            this.blackWhite.Name = "blackWhite";
+            this.blackWhite.Size = new System.Drawing.Size(107, 25);
+            this.blackWhite.TabIndex = 1;
+            this.blackWhite.Text = "В черно-белый формат";
+            this.blackWhite.UseVisualStyleBackColor = true;
+            this.blackWhite.Click += new System.EventHandler(this.blackWhite_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(386, 52);
+            this.trackBar1.Maximum = 6;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(194, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(395, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(489, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 22);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(395, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 22);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(489, 153);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 22);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(395, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 22);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Green;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(489, 181);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 22);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 442);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.blackWhite);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.mainPctBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPctBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPctBox;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button blackWhite;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic_Editor
+{
+
+    public partial class Form1 : Form
+    {
+        private Point previosPoint, point;
+        private Bitmap bitmapImage;
+        private Pen blackPen;
+        private Graphics graphics;
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = 
+                "Image files (*.BMP, *.JPG, " + "*.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+            if(DialogResult.OK == openFileDialog.ShowDialog())
+            {
+                Image image = Image.FromFile(openFileDialog.FileName);
+                int width = image.Width;
+                int height = image.Height;
+                //mainPctBox.Width = width;
+                //mainPctBox.Height = height;
+                bitmapImage = new Bitmap(image, mainPctBox.Width, mainPctBox.Height);
+                mainPctBox.Image = bitmapImage;
+                graphics= Graphics.FromImage(mainPctBox.Image);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blackPen = new Pen(Color.Black, 1);
+        }
+
+        private void mainPctBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            previosPoint.X = (int)e.X;
+            previosPoint.Y = (int)e.Y;
+        }
+
+        private void mainPctBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                point.X = (int)e.X;
+                point.Y = (int)e.Y;
+                graphics.DrawLine(blackPen, previosPoint, point);
+
+                previosPoint = point;
+
+                mainPctBox.Invalidate();
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Сохранить картинку как ";
+            saveFileDialog.OverwritePrompt= false;
+            saveFileDialog.CheckFileExists=false;
+            saveFileDialog.Filter = 
+                "Bitmap File(*.bmp)|*.bmp|" + 
+                "GIF File(*.gif)|*.gif|" +
+                "JPEG File(*.jpg)|*.jpg|" + 
+                "PNG File(*.png)|*.png";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                string filExtn = fileName.Remove(0, fileName.Length - 3);
+
+                switch (filExtn)
+                {
+                    case "bmp":
+                        bitmapImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "jpg":
+                        bitmapImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        bitmapImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif); break;
+                    case "tif":
+                        bitmapImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        bitmapImage.Save(fileName, System.Drawing.Imaging.ImageFormat.Png); break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void blackWhite_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bitmapImage.Width; i++)
+            {
+                for (int j = 0; j < bitmapImage.Height; j++)
+                {
+                    int R = bitmapImage.GetPixel(i, j).R;
+                    int G = bitmapImage.GetPixel(i, j).R;
+                    int B = bitmapImage.GetPixel(i, j).R;
+                    int Gray = (R + G + B) / 3;
+
+                    Color p = Color.FromArgb(50, Gray, Gray, Gray);
+
+                    bitmapImage.SetPixel(i, j, p);
+                }
+            }
+            Refresh();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+    }
+}
